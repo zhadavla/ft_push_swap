038751c715cb9b4a9bc3b6e6e8f8167f0be118ec
Vladyslav Zhadan <zhadavla@fit.cvut.cz>
2023-07-02 15:47:20 +0200

    libft

---
+++ src/libft/libft.h
@@ -0,0 +1,12 @@
+#ifndef PUSH_SWAP_LIBFT_H
+#define PUSH_SWAP_LIBFT_H
+
+#include "stdlib.h"
+
+int	ft_atoi(const char *nptr);
+size_t	ft_strlen(const char *s);
+int *chars_to_ints(char **argv, int len);
+char	*ft_itoa(int n);
+char	*ft_strdup(const char *s);
+
+#endif //PUSH_SWAP_LIBFT_H
