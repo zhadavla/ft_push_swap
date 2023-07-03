edb9b39cf16364da3f7cc1586aca3eeb3fe21e3b
Vladyslav Zhadan <zhadavla@fit.cvut.cz>
2023-07-03 22:41:57 +0200

    header

---
+++ src/push_swap.h
@@ -0,0 +1,13 @@
+#ifndef FT_PUSH_SWAP_PUSH_SWAP_H
+#define FT_PUSH_SWAP_PUSH_SWAP_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "ft_stack/stack.h"
+#include "libft/libft.h"
+#include <string.h>
+
+
+
+#endif //FT_PUSH_SWAP_PUSH_SWAP_H
